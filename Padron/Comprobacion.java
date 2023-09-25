260f3e24dcc65554bae749a6f159eebee985d5e3
rubencerroo <ruben.cerrodiaz@usp.ceu.es>
2023-09-25 11:00:38 +0200

    Padron1

---
+++ Padron/Comprobacion.java
@@ -0,0 +1,12 @@
+package Padron;
+
+public class Comprobacion {
+    public static void realizarComprobacion() {
+        Habitante habitante1 = new Habitante("Juan", "Torres", "Sanz");
+        System.out.println(habitante1.getNombre() + " " + habitante1.getApellido1() + " " + habitante1.getApellido2());
+    }
+
+    public static void main(String[] args) {
+        realizarComprobacion();
+    }
+}
